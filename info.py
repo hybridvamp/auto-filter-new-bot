9282cd3a2b5bf5b1c7b8a3692bf03d036e51f944
adityabotdeployer <120628250+adityabotdeployer@users.noreply.github.com>
2023-01-18 22:45:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -78,7 +78,7 @@ COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Aditgbot:0987@cluster0.wutyhoa.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
-MONGO_URL = os.environ.get('MONGO_URL', "")
+MONGO_URL = os.environ.get('MONGO_URL', "mongodb+srv://Aditgbot:0987@cluster0.wutyhoa.mongodb.net/?retryWrites=true&w=majority")
 
 #Downloader
 DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
